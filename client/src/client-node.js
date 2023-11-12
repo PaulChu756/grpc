2bb532d3c0021778093dafd864a280b4ea5c1ae1
Paul <paulchu756@gmail.com>
2023-11-12 00:48:47 -0600

    second fix

--- client/src/client-node.js
+++ client/src/client-node.js
@@ -1,11 +1,9 @@
 const PROTO_PATH = __dirname + '/../../protos/keysearch/proto/keysearch.proto';
-// IP Address + Port #
 const SOCKET = 'localhost:50051';
 
 const grpc = require('@grpc/grpc-js');
 const protoLoader = require('@grpc/proto-loader');
 
-// Some options for loading .proto file
 const options = {
     keepCase: true,
     longs: String,
@@ -14,28 +12,19 @@ const options = {
     oneofs: true,
 };
 
-// Load the .proto file
 const packageDefinition = protoLoader.loadSync(PROTO_PATH, options);
-// Load the proto package 'proto.keysearch' into constant keysearchProto
-// This allows keysearchProto to recognize all of the definitions in the .proto file
 const keysearchProto = grpc.loadPackageDefinition(packageDefinition).proto.keysearch;
 
-function main(){
-    // Simple error catching
-    // if(process.argv[3 === undefined]){
-        if (process.argv.length !== 5 || (process.argv[2] !== 'whohas2' && process.argv[2] !== '"whohas2"')) {
-            return console.log('Usage: make client-node ARGS="whohas <word>"');
-        }
-        
+function main() {
+    if (process.argv.length < 4) {
+        return console.log('Usage: make client-node ARGS="whohas <word>" or "whohas2 <word> <threshold>"');
+    }
 
-    // Parse user's arguments
     var arg = process.argv.slice(2);
     var func;
     var word = arg[1];
-    
-    
-    // Determine which function user's calling
-    switch(arg[0]){
+
+    switch (arg[0]) {
         case 'whohas':
             func = 'Whohas';
             break;
@@ -43,19 +32,40 @@ function main(){
             func = 'Whohas2';
             break;
         default:
-            console.log('\nUsage: make client-node ARGS="whohas <word>"\n');
+            console.log('\nUsage: make client-node ARGS="whohas <word>" or "whohas2 <word> <threshold>"\n');
             process.exit(1);
-    };
+    }
 
-    // Create the client stub
-    // This allows us to call the 'Whohas' function defined in the 'KeywordSearch' service
-    // Additionally, send requests to port 50051
+    if (func === 'Whohas2') {
+        // Check if enough arguments are provided for 'whohas2'
+        if (arg.length !== 4) {
+            console.log('Usage: make client-node ARGS="whohas2 <word> <threshold>"');
+            process.exit(1);
+        }
+
+        // Parse the threshold value
+        var threshold = parseInt(arg[3]);
+        if (isNaN(threshold)) {
+            console.log('Threshold must be a number.');
+            process.exit(1);
+        }
+    }
+
     const client = new keysearchProto.KeywordSearch(SOCKET, grpc.credentials.createInsecure());
-    // Finally, call the function
-    client[func]({word: word}, (err, response) => {
+    const request = { word: word };
+
+    if (func === 'Whohas2') {
+        request.threshold = threshold;
+    }
+
+    client[func](request, (err, response) => {
+        if (err) {
+            console.error(err);
+            process.exit(1);
+        }
         console.log(response.Results);
         process.exit(0);
     });
-};
+}
 
 main();
